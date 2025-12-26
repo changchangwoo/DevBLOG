0b441b1b4ac3adbfd38c9cd7dd332d2e633cfcac
changchangwoo <changchangwoo@naver.com>
2025-12-27 00:11:09 +0900

    Refactor : author 텍스트 수정

--- lib/author.ts
+++ lib/author.ts
@@ -12,7 +12,7 @@ export interface AuthorInfo {
 export const AUTHOR_INFO: AuthorInfo = {
   name: "이창우",
   role: "프론트엔드 개발자",
-  bio: "프로젝트를 좋아하는 주니어 개발자입니다.\n부족하더라도 항상 씩씩한 사람이 되고 싶습니다.",
+  bio: "프로젝트를 좋아하는 개발자입니다.\n부족하더라도 씩씩한 사람이 되고 싶습니다.",
   profileImage: "/images/common/profile_img.png",
   links: {
     github: "https://github.com/changchangwoo",
